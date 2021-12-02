d117ab59d1f059bfd0a9b5a127916c89da929b0a
ichelnokov <i.chelnokov@mykedr.ru>
2021-12-02 10:26:13 +0300

    add sender form in ConferenceController

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,6 +8,9 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +18,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route ("/", name="home")
      * @param ConferenceRepository $conferenceRepository
@@ -25,6 +35,7 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig', [
            'conferences'=> $conferenceRepository->findAll()
         ]);
+
     }
 
     /**
@@ -33,11 +44,20 @@ class ConferenceController extends AbstractController
      * @param Conference $conference
      * @param CommentRepository $commentRepository
      * @return Response
+     * @throws ORMException
      */
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository):Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $comment->setConference($conference);
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('conference', ['slug'=>$conference->getSlug()]);
+        }
+
 
         $offset = max(0, $request->query->getInt('offset',0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
