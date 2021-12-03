f19cd0dc74d540046627f1639b0b50f26f197ff1
ichelnokov <i.chelnokov@mykedr.ru>
2021-12-03 13:39:49 +0300

    some change CommentMessageHandler

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -7,6 +7,8 @@ use App\Repository\CommentRepository;
 use App\Service\SpamCheckerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -22,13 +24,17 @@ class CommentMessageHandler implements MessageHandlerInterface
     private MessageBusInterface $bus;
     private WorkflowInterface $workflow;
     private LoggerInterface $logger;
+    private string $adminEmail;
+    private MailerInterface $mailer;
 
     public function __construct(EntityManagerInterface $entityManager,
                                 CommentRepository $commentRepository,
                                 SpamCheckerService $spamCheckerService,
                                 MessageBusInterface $bus,
                                 WorkflowInterface $commentStateMachine,
-                                LoggerInterface $logger
+                                LoggerInterface $logger,
+                                MailerInterface $mailer,
+                                string $adminEmail
     )
     {
         $this->entityManager = $entityManager;
@@ -37,12 +43,15 @@ class CommentMessageHandler implements MessageHandlerInterface
         $this->bus = $bus;
         $this-> workflow = $commentStateMachine;
         $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
     }
 
     /**
      * @throws ServerExceptionInterface
      * @throws RedirectionExceptionInterface
      * @throws ClientExceptionInterface
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
     public function __invoke(CommentMessage $message)
     {
@@ -52,14 +61,15 @@ class CommentMessageHandler implements MessageHandlerInterface
         }
 
         if ($this->workflow->can($comment, 'accepts')){
-            $status = $this->spamCheckerService->getSpamCheck($comment) ? 'ham' : 'spam';
+            $status = $this->spamCheckerService->getSpamCheck($comment) ? 'confirmation' : 'spam';
             $transition = 'accepts';
             if ('spam' === $status){
                 $transition = 'reject_spam';
             }
             $this->workflow->apply($comment, $transition);
             $this->entityManager->flush();
-            $this->bus->dispatch($message);
+//            $this->bus->dispatch($message);
+
         } elseif($this->workflow->can($comment, 'publish')) {
             $this->workflow->apply($comment, 'published');
         }else{
