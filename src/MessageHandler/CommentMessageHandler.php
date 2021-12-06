b5e16ea54bd073eea677e2c751886dc263fc6d81
ichelnokov <i.chelnokov@mykedr.ru>
2021-12-06 15:50:07 +0300

    fix browser notification

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -12,8 +12,6 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Notifier\Notification\Notification;
-use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -26,7 +24,9 @@ class CommentMessageHandler implements MessageHandlerInterface
     private SpamCheckerService $spamCheckerService;
     private MessageBusInterface $bus;
     private WorkflowInterface $workflow;
-    private NotifierInterface $notifier;
+    private LoggerInterface $logger;
+    private string $adminEmail;
+    private MailerInterface $mailer;
     private LoggerService $loggerService;
 
     public function __construct(EntityManagerInterface $entityManager,
@@ -34,7 +34,9 @@ class CommentMessageHandler implements MessageHandlerInterface
                                 SpamCheckerService $spamCheckerService,
                                 MessageBusInterface $bus,
                                 WorkflowInterface $commentStateMachine,
-                                NotifierInterface $notifier,
+                                LoggerInterface $logger,
+                                MailerInterface $mailer,
+                                string $adminEmail,
                                 LoggerService $loggerService,
 
     )
@@ -44,8 +46,10 @@ class CommentMessageHandler implements MessageHandlerInterface
         $this->spamCheckerService = $spamCheckerService;
         $this->bus = $bus;
         $this-> workflow = $commentStateMachine;
+        $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
         $this->loggerService = $loggerService;
-        $this->notifier = $notifier;
     }
 
     /**
@@ -75,9 +79,8 @@ class CommentMessageHandler implements MessageHandlerInterface
         } elseif($this->workflow->can($comment, 'publish')) {
             $status = 'published';
             $this->workflow->apply($comment, $status);
-            $this->notifier->send(new CommentReviewNotification($comment),...$this->notifier->getAdminRecipients());
         }else{
-//            $this->logger->bebug('Dropping comment message', ['comment'=>$comment->getId(), 'state'=>$comment->getState()]);
+            $this->logger->bebug('Dropping comment message', ['comment'=>$comment->getId(), 'state'=>$comment->getState()]);
         }
 
         $log_data = [
