ad424f28ffcbd485f721bc892461edc26bfe01f2
ichelnokov <i.chelnokov@mykedr.ru>
2021-12-07 08:05:39 +0300

    fix FilterApi

--- src/Api/FilterPublishedCommentQueryExtension.php
+++ src/Api/FilterPublishedCommentQueryExtension.php
@@ -5,6 +5,7 @@ namespace App\Api;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Comment;
 use Doctrine\ORM\QueryBuilder;
 
 class FilterPublishedCommentQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
@@ -13,8 +14,7 @@ class FilterPublishedCommentQueryExtension implements QueryCollectionExtensionIn
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         if(Comment::class === $resourceClass){
-            /** @var TYPE_NAME $queryBuilder */
-            $queryBuilder->andWhere(sprinf("%s.state = 'published'", $queryBuilder->getRootAliases()[0]));
+            $queryBuilder->andWhere(sprintf("%s.state = 'published'", $queryBuilder->getRootAliases()[0]));
         }
     }
 
@@ -24,4 +24,5 @@ class FilterPublishedCommentQueryExtension implements QueryCollectionExtensionIn
             $queryBuilder->andWhere(sprintf("%s.state = 'published'", $queryBuilder->getRootAliases()[0]));
         }
     }
+
 }
